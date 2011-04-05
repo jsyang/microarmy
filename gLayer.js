46c3972efcccb43344bc3d508d35b496ce66c649
Jim Yang <jsyang.ca@gmail.com>
2011-04-05 02:16:43 -0400

    Some minor changes logged in Dec. 2010.

--- gLayer.js
+++ gLayer.js
@@ -12,34 +12,34 @@
 	
 *******************************************************************************/
 function gLayer(imgs,size,fn){
-	this.i=[];
-	this.iLoaded=0;
-	
-	this.callback=fn;
-	
-	this.checkProgress=function(obj,n){
-		if(++obj.iLoaded==n) obj.callback(obj);
-	};
-
-	
-	this.e=document.createElement("canvas");
-	if(size!=undefined){
-		this.e.width=size[0];
-		this.e.height=size[1];
-	}
-	this.c=this.e.getContext('2d');
-	
-	document.body.appendChild(this.e);
-
-	for(var j in imgs){
-		var a=new Image();
-		a.src=imgs[j];
-		this.i.push(a);				
-		a.onerror=function(){ alert("Error in loading "+this.src); };
-		if(fn!=undefined) a.onload=this.checkProgress(this,imgs.length);
-	}
-	
-	if(!imgs.length) fn();
+    this.i=[];
+    this.iLoaded=0;
+    
+    this.callback=fn;
+    
+    this.checkProgress=function(obj,n){
+        if(++obj.iLoaded==n) obj.callback(obj);
+    };
+    
+    
+    this.e=document.createElement("canvas");
+    if(size!=undefined){
+        this.e.width=size[0];
+        this.e.height=size[1];
+    }
+    this.c=this.e.getContext('2d');
+    
+    document.body.appendChild(this.e);
+    
+    for(var j in imgs){
+        var a=new Image();
+        a.src=imgs[j];
+        this.i.push(a);				
+        a.onerror=function(){ alert("Error in loading "+this.src); };
+        if(fn!=undefined) a.onload=this.checkProgress(this,imgs.length);
+    }
+    
+    if(!imgs.length) fn();
 }
 
 
@@ -49,17 +49,17 @@ function gLayer(imgs,size,fn){
 
 *******************************************************************************/
 function getDocumentSize(){
-	var m=Math.max;
-	var db=document.body;
-	var de=document.documentElement;
-
-	return [
-		m(db.scrollWidth,db.offsetWidth,
-		  de.clientWidth,de.scrollWidth,
-		  de.offsetWidth),
-
-		m(db.scrollHeight,db.offsetHeight,
-		  de.clientHeight,de.scrollHeight,
-		  de.offsetHeight)
-	];
+        var m=Math.max;
+        var db=document.body;
+        var de=document.documentElement;
+        
+        return [
+            m(db.scrollWidth,db.offsetWidth,
+              de.clientWidth,de.scrollWidth,
+              de.offsetWidth),
+        
+            m(db.scrollHeight,db.offsetHeight,
+              de.clientHeight,de.scrollHeight,
+              de.offsetHeight)
+        ];
 }
