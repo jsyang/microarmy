0c01af75d6f55e1ccbacd3968562c6ee5ee90118
jsyang <jsyang.ca@gmail.com>
2012-01-06 01:57:21 -0500

    moved vehicle AI to behavior tree! created a shorthand syntax for behavior trees!

--- entity_behavior.js
+++ entity_behavior.js
@@ -1,76 +1,218 @@
 // Behavior tree implementation here ///////////////////////////////////////////
 var Behavior={
-  
-  RESULT:{
-    OK:0,     // continue deeper
-    TICK:1,   // bail out of all levels
-    FAIL:2
-  },
-  
-  /*
-  behavior tree json...:
-  {
-    id:   'name of strat',
-    strat:'name of execute token',
-    children:[]
-  }
-  */
-  
+
   Execute:function(tree,thisArg){
     if(!tree.id) return alert('No strategy specified!');
     switch(tree.id) {
       
-      case 'sequence':  // Quit on first FAIL
+      case 'sequence':  // Quit on first false
         for(var i=0; i<tree.children.length; i++)
-          if(Behavior.Execute(tree.children[i],thisArg)==Behavior.RESULT.FAIL)
-            return Behavior.RESULT.FAIL;
-        return Behavior.RESULT.OK;
+          if(!Behavior.Execute(tree.children[i],thisArg)) return false;
+        return true;
         break;
       
-      case 'selector':  // Quit on first SUCCESS
+      case 'selector':  // Quit on first true
         for(var i=0; i<tree.children.length; i++)
-          if(Behavior.Execute(tree.children[i],thisArg)==Behavior.RESULT.OK)
-            return Behavior.RESULT.OK;
-        return Behavior.RESULT.FAIL;
+          if(Behavior.Execute(tree.children[i],thisArg)) return true;
+        return false;
         break;
       
       case 'lookup':    // to do: lookup decorator
         break;
       
       default:          // Custom node behavior and decorators.
-        if(!Behavior.Custom[tree.id])
-          return alert('Custom behavior not found!');
-        var result=Behavior.Custom[tree.id](thisArg);
-        if(tree.children && result==Behavior.RESULT.OK)
-          return Behavior.Execute(tree.children[0]);
-        else
-          return result;
+        var realId=tree.id.charAt(0)==='!'?tree.id.slice(1):tree.id;
+        if(!Behavior.Custom[realId])
+          return alert('Custom behavior not found!');        
+        return realId==tree.id?
+          Behavior.Custom[realId](thisArg)
+          : !Behavior.Custom[realId](thisArg);
     }
     return alert('ERROR: You are not supposed to see this!');
+  },
+
+/* notes on behavior tree shorthand:
+
+() = selector
+<> = sequence
+!token = run the decorator but return with logically opposite result
+
+you don't actually need chained decorators, since you can just use a sequence
+and inverts to do the same thing
+
+decorator-filter1
+ decorator-filter2
+   decorator-filter3
+     shootMachineGun
+
+is identical to
+<decorator-filter1,decorator-filter2,decorator-filter3>
+
+should not allow stuff to happen during corpse time, which is what
+the if(isDead) clause catches.
+if(isDead) { doCorpsething; } else: (do stuff in the behavior tree)
+
+*/  
+  
+  ConvertShortHand:function(code){
+    return code
+    .replace(/\[/g,'{id:"')
+    .replace(/\]/g,'"}')
+    .replace(/\(/g,'{id:"selector",children:[')
+    .replace(/</g,'{id:"sequence",children:[')
+    .replace(/>/g,']}')
+    .replace(/\)/g,']}');
   },
   
 // Custom decorators and tasks /////////////////////////////////////////////////
   Custom:{
     
     isDead:function(obj) {
-      return obj._.health<=0? Behavior.RESULT.OK : Behavior.RESULT.FAIL;
+      return obj._.health<=0;
     },
     
     isOutsideWorld:function(obj) {
-      return world.isOutside(this)? Behavior.RESULT.OK : Behavior.RESULT.FAIL;
+      return world.isOutside(obj);
     },
     
-    loopAnimation:function(obj) {
-      if(++_.frame.current>_.frame.last) _.frame.current=_.frame.first;
-      return Behavior.RESULT.OK;
+    isReloading:function(obj) { var _=obj._;
+      if(_.reload.ing) {
+        _.reload.ing--;
+        return true;
+      } else if(_.ammo.clip==0) {
+        _.reload.ing=_.reload.time;
+        _.ammo.clip=_.ammo.max;
+        return true;
+      }
+      return false;
+    },
+    
+    isVehicleMoving:function(obj) { var _=obj._;
+      return  _.action==VEHICLE.ACTION.MOVING ||
+              _.action==VEHICLE.ACTION.TURNING;
+    },
+    
+    loopAnimation:function(obj) { var _=obj._;
+      if(++_.frame.current>_.frame.last)
+        _.frame.current=_.frame.first;
+      return true;
+    },
+    
+    seeTarget:function(obj) { var t=obj._.target;
+      return t? !(Math.abs(t.x-obj.x)>>obj._.sight) : false;
+    },
+    
+        
+    foundTarget:function(obj) { var t=obj._.target, _=obj._;
+      // Try to find a valid target!
+      if(!t || Behavior.Custom.isDead(t) ||
+         !Behavior.Custom.seeTarget(obj) || t.team==obj.team) {
+        _.target=undefined;
+        var h=world.xHash.getNBucketsByCoord(obj.x,(_.sight-5)*2+2)
+        for(var i=0, minDist=Infinity; i<h.length; i++) {
+          if(h[i].team==obj.team) continue;
+          if(Behavior.Custom.isDead(h[i])) continue;
+          var dist=Math.abs(h[i].x-obj.x);
+          if(dist>>_.sight) continue;
+          if(dist<minDist){ _.target=h[i]; minDist=dist; }
+        }
+      }
+      return _.target? true:false;
+    },
+    
+    // Only for Vehicles -- Handle rotation to face target
+    isFacingTarget:function(obj) { var _=obj._;
+      if(_.target && (_.target.x-obj.x)*_.direction<0) {
+        if(!_.turn.ing) {            
+          _.turn.ing=1;
+          _.turn.current=0;
+          _.action=VEHICLE.ACTION.TURNING;
+        } else {
+          _.turn.current++;
+          if(_.turn.current>_.turn.last) {
+            _.turn.ing=_.turn.current=0;
+            _.direction*=-1;
+            _.action=VEHICLE.ACTION.MOVING;
+          }            
+        }
+        return false;
+      }
+      return true;
     },
     
     movePawn:function(obj) {
       obj.x+=obj._.direction;
       obj.y=world.getHeight(obj.x);
-    }
+      return true;
+    },
     
+    // this attack function came from Vehicle class.
+    attack:function(obj) { var _=obj._;
+      //if(!_.projectile) return true;
+      var dist=Math.abs(_.target.x-obj.x);      
+      
+      var accuracy=[0,0]; // chance to hit, [periphery, target bonus]
+      var strayDY=0;      // deviation in firing angle.
+      var fSpeed=0;       // projectile speed.
+      
+      if(_.projectile==MGBullet) {
+        if(_.ammo.clip==_.ammo.max) soundManager.play('mgburst');
+        accuracy=[0.65,0.35]; strayDY=$.R(-15,15)/100; fSpeed=4;
+      }
+      
+      // Projectile origin relative to sprite
+      var pDY=-obj.img.h>>1;
+      var pDX=_.direction*4;
+      
+      // Distance penalties for chance to hit
+      // this should probably be moved inside the projectile class
+      if(dist>50){  accuracy[0]-=0.02; accuracy[1]-=0.15; }
+      if(dist>120){ accuracy[0]-=0.01; accuracy[1]-=0.18; }
+      if(dist>180){ accuracy[0]-=0.01; accuracy[1]-=0.08; }
+      if(dist>200){ accuracy[0]-=0.01; accuracy[1]-=0.08; }
+      
+      // Flight speed = 1<<2.
+      world.addPawn(
+        new _.projectile(
+          obj.x+pDX,obj.y+pDY,
+          obj.team,
+          _.target,
+          _.direction*4,
+          ((_.target.y-(_.target.img.h>>1)-(obj.y-(obj.img.h>>1))-pDY)*4)/dist+strayDY,
+          accuracy
+        )
+      );
+      _.ammo.clip--;
+      return true;
+    },
+    
+    walkingOffMapCheck:function(obj) {
+      if(TEAM.GOALDIRECTION[obj.team]==obj._.direction) {
+        soundManager.play('accomp');
+        world.pause();
+      }
+      // just disappear, walked off the map
+      obj.remove();
+      return true;
+    }
   }
 };
 
 
+
+
+function testConversion(){
+var a="\
+(\n\
+  [isReloading],\n\
+  <[foundTarget],(\n\
+    <[!isFacingTarget],[loopAnimation]>,\n\
+    <[seeTarget],[attack]>\n\
+  )>,\n\
+  <[movePawn],[loopAnimation],<\n\
+    [isOutsideWorld],\n\
+    [walkingOffMapCheck]\n\
+  >>\n\
+)";
+return Behavior.ConvertShortHand(a);
+}
--- entity_vehicle.js
+++ entity_vehicle.js
@@ -13,83 +13,7 @@ var VEHICLE={
 
 Vehicle.prototype=new Pawn;
 function Vehicle() {
-  
-  this.seeTarget=function(returnDist) { var _=this._;
-    return _.target?
-      returnDist?
-        Math.abs(_.target.x-this.x)
-        : !(Math.abs(_.target.x-this.x)>>_.sight)
-      : 0;
-  };
-  
-  this.findTarget=function() { var _=this._;
-    _.target=undefined;
-    // Get all objects possibly within our sight, don't care about dist
-    var h=world.xHash.getNBucketsByCoord(this.x,(_.sight-5)*2+2)
-    for(var i=0, minDist=Infinity; i<h.length; i++) {
-      if(h[i].team==this.team) continue;
-      if(h[i].isDead()) continue;                   // already dead!
-      if(Math.abs(h[i].x-this.x)>>_.sight) continue;   // can't see closest!      
-      if(Math.abs(h[i].x-this.x)<minDist){
-        _.target=h[i]; minDist=Math.abs(h[i].x-this.x);
-      }
-    }    
-  }
-  
-  this.move=function() { var _=this._;
-    this.x+=_.direction;
-    this.y=world.getHeight(this.x);
-    if(!world.isOutside(this)) { return true; }
-    else if(TEAM.GOALDIRECTION[this.team]==_.direction) {
-      soundManager.play('accomp');
-      world.pause();
-    }
-    // just disappear, walked off the map
-    this.remove();
-    return false;
-  }
-  
-  this.attack=function() { var _=this._;
-    if(!_.projectile) return true;
-    var distTarget=this.seeTarget(1);
-    
-    // Melee distance: LESS than one body!
-    // no melee for vehicles
-    
-    var accuracy=[0,0]; // chance to hit, [periphery, target bonus]
-    var strayDY=0;      // deviation in firing angle.
-    var fSpeed=0;
-    if(_.projectile==MGBullet) {
-      if(_.ammo.clip==_.ammo.max) soundManager.play('mgburst');
-      accuracy=[0.65,0.35]; strayDY=$.R(-15,15)/100; fSpeed=4;
-    }
-    
-    // Projectile origin relative to sprite
-    var pDY=-this.img.h>>1;
-    var pDX=_.direction*4;
-    
-    // Distance penalties for chance to hit
-    // this should probably be moved inside the projectile class
-    if(distTarget>50){  accuracy[0]-=0.02; accuracy[1]-=0.15; }
-    if(distTarget>120){ accuracy[0]-=0.01; accuracy[1]-=0.18; }
-    if(distTarget>180){ accuracy[0]-=0.01; accuracy[1]-=0.08; }
-    if(distTarget>200){ accuracy[0]-=0.01; accuracy[1]-=0.08; }
-    
-    // Flight speed = 1<<2.
-    world.addPawn(
-      new _.projectile(
-        this.x+pDX,this.y+pDY,
-        this.team,
-        _.target,
-        _.direction*4,
-        ((_.target.y-(_.target.img.h>>1)-(this.y-(this.img.h>>1))-pDY)*4)/distTarget+strayDY,
-        accuracy
-      )
-    );
-    _.ammo.clip--;
-    return true;
-  }
-  
+
   this.corpsetime=240;
   this.takeDamage=function(d){ return this._.health-=d; };
   this.isDead=function(){ return this._.health<=0; };
@@ -118,47 +42,14 @@ function Vehicle() {
   };
 
   this.alive=function(){ var _=this._;
-    if(this.isDead()) {
+    if(Behavior.Custom.isDead(this)) {
       if(_.action<VEHICLE.ACTION.WRECK) {
         _.action=VEHICLE.ACTION.WRECK;
         //soundManager.play('die1,die2,die3,die4'.split(',')[$.R(0,3)]);        
       }
       return false;
     } else {
-    
-      // If reloading, don't do anything else.     
-      if(_.reload.ing) { _.reload.ing--; return true; }
-      else if(_.ammo.clip==0) {
-        _.reload.ing=_.reload.time;
-        _.ammo.clip=_.ammo.max;
-        return true;
-      }
-      
-      // Try to find a valid target!
-      if(!_.target || _.target.isDead() ||
-         !this.seeTarget() || _.target.team==this.team) this.findTarget();
-      
-      if(_.target) {
-        // Handle rotation
-        if(_.target && (_.target.x-this.x)*_.direction<0) {
-          if(!_.turn.ing) {            
-            _.turn.ing=1;
-            _.turn.current=0;
-          } else {
-            _.turn.current++;
-            if(_.turn.current>_.turn.last) {
-              _.turn.ing=_.turn.current=0;
-              _.direction*=-1;
-            }            
-          }
-        } else return this.attack();
-      } else this.move();
-      
-      // Animation loop -- only for moving or turning, not attacking
-      if(_.action==VEHICLE.ACTION.MOVING || _.action==VEHICLE.ACTION.TURNING)
-        if(++_.frame.current>_.frame.last)
-          _.frame.current=_.frame.first;
-  
+      Behavior.Execute(this.behavior,this);
       return true;
     }
   };  
@@ -191,21 +82,15 @@ function APC(x,y,team) {
     ammo:       { clip:6, max:6 }
   };
   
-  this.btree={
-    id:'selector', 
-    children:[
-      {id:'isDead'},
-      {
-        id:'selector',
-        children:[
-          {id:'isOutsideWorld'},
-          {id:'movePawn'}
-        ]
-      }
-    ]
-  };
-  
-  this.alive=function(){
-    return Behavior.Execute(this.btree,this);
-  }
+  this.behavior={id:"selector",children:[
+  {id:"isReloading"},
+  {id:"sequence",children:[{id:"foundTarget"},{id:"selector",children:[
+    {id:"sequence",children:[{id:"!isFacingTarget"},{id:"loopAnimation"}]},
+    {id:"sequence",children:[{id:"seeTarget"},{id:"attack"}]}
+  ]}]},
+  {id:"sequence",children:[{id:"movePawn"},{id:"loopAnimation"},{id:"sequence",children:[
+    {id:"isOutsideWorld"},
+    {id:"walkingOffMapCheck"}
+  ]}]}
+]};
 }
