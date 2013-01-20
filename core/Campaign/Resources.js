c1a3341e5ae8bef0181843f25fb2f4a968a1cfe1
jim@imgur <jimgur@jimimgurs-MacBook-Air.local>
2013-01-19 19:58:16 -0800

    more notes, and an LCG for use with randomness testing

--- core/Campaign/Resources.js
+++ core/Campaign/Resources.js
@@ -5,36 +5,36 @@ define([
 ],function(Class, $){
   // elements
   // atom is indivisible
-  
+
   // Each resource's worth in terms of atoms
   // and chance of synthesis, given all the ingredients
-  
+
   // todo: load this externally to define what our resources are.
-  
-  
+
+
   var ResourcesTree = {
-        
+
     'nutrients' : {
       atoms : 18,
       synth : 0.6
     },
-    
+
     'metal' : {
       atoms : 5,
       synth : 0.2
     },
-    
+
     'combustible' : {
       atoms : 11,
       synth : 0.1
     },
-    
+
     'rubble' : {
       atoms : 2,
       synth : 1
     },
-    
-    
+
+
     // depends on metal tree:
     'alloy' : {
       synth : 0.8,
@@ -42,14 +42,14 @@ define([
         'metal' : 2
       }
     },
-    
+
     'advancedalloy' : {
       synth : 0.3,
       needs : {
         'alloy' : 4
       }
     },
-    
+
     'food' : {
       // needed to create new infantry
       // needed to heal infantry
@@ -58,7 +58,7 @@ define([
         'nutrients' : 3
       }
     },
-    
+
     'explosives' : {
       synth : 0.3,
       needs : {
@@ -66,65 +66,73 @@ define([
         'nutrients'   : 3
       }
     },
-    
+
+
+    // all actor resources have an additional morale rating?
+    // 0 to 1
     'combatant' : {
       synth : 0.03,
+      morale : 0.9,
       needs : {
         'food'      : 3,
         'citizens'  : 1,
         'smallarms' : 1
       }
+    },
+
+    'citizen' : {
+    
     }
-    
+
   };
-    
+
   /*
-   
+
   each resource has its own harvester and synthesizer
   synthesizer (entity which processes ingredients and tries to synthesize another resource)
-  
+
   for instance a production base will have a small arms factory
   small arms factory synthesizes small arms parts    from metal, minerals
                      synthesizes small arms ammo     from explosives, small arms parts
                      synthesizes small arms          from small arms parts
-  
+
   crops synthesizes food from nutrients
-  
+
   world (natural processes) synthesizes nutrients from atoms
-  
-  
+
+
   atomizer can turn non-atom resources back into atoms
   universal constructor can use those atoms to build whatever you have researched to build
      bypassing hierarchical resource requirements
-  
+
   certain synthesizers have bonuses for producing certain things, otherwise they use the default
   synthesis rate
-  
+
   map tiles have a certain number of atoms (the height, ex: 14 --> 14e6 atoms)
   and also a certain number of pre-existing resource amounts
-  
+
   forest tile --> 32 nutrients
                   6 metal
                   18 food
-  
+
   a synthesizing entity can have multiple synthesis processes going on at once
-  
+
   determine the atomic value of each non-elementary resource by
   recursing through the needs tree until you get to an elementary resource
-  
+
   mining a piece of terrain (harvesting using a harvester entity) will yield resources
   farming (harvesting)
-  
+
   harvesting is basically a high level synthesis, so in actuality you're doing the same thing
-  
+
   if a high level synthesis fails the synthesis chance roll, there is a chance the ingredients
   will either be lost or turned into rubble
-  
+
   rubble fills up resource stores eventually and must be cleared (maybe automatically)
-  
+
   */
   var Resource = Class.extend({
-    
+
     init : function(params) {
       this._ = $.extend({
         atoms : 0,
@@ -132,6 +140,6 @@ define([
       }, params);
     }
   });
-  
+
   return Resource;
 });
--- core/util/$.js
+++ core/util/$.js
@@ -1,9 +1,45 @@
 // $ -- misc util functions
 define({
 
-  // Pick a random element from an array
-  pickRandom : function(a) { return a[(Math.random()*a.length)>>0]; },
-  
+  // Random float / with coefficient
+  r : function(n) { return n?n*Math.random():Math.random(); },
+
+  // Random integer from [min,max] (bound inclusive)
+  R : function(min,max) { return min+(Math.random()*(max-min+1))>>0; },
+
+  // Returns a PseudoRandom function using a seed
+  PRfunc : function(seed){
+    var LCG = {
+      Xi    : seed? seed : 0,
+      seed  : seed? seed : 0,
+      c     : 17,
+      a     : 16807,
+      m     : 524287 // mersenne prime #7
+      // conditions are fulfilled for achieving a full period.
+    };
+
+    return function LCGpseudorandom(min,max) {
+      LCG.Xi = (LCG.a*LCG.Xi+LCG.c) % LCG.m;
+      return min +(LCG.Xi/LCG.m*(max-min+1))>>0;
+    };
+  },
+
+  // Pick a random element from an array (optional custom random func)
+  pickRandom : function(a, randomFunc) {
+    if(randomFunc) {
+      return a[randomFunc(0,a.length-1)];
+    } else {
+      return a[(Math.random()*a.length)>>0];
+    }
+  },
+
+  // Array sum
+  sum : function(a) { for(var sum=0, i=a.length; i--; sum+=a[i] ); return sum; },
+
+  // Object attribute sum
+  sumObj : function(a) { var sum=0; for(var i in a) sum+=a[i]; return sum; },
+
+
   // Checks if any arguments are undefined
   isUndefined : function() {
     for(var i=0; i<arguments.length; i++) {
@@ -11,21 +47,9 @@ define({
         return true;
       }
     }
-    
+
     return false;
   },
-  
-  // Random float / with coefficient
-  r : function(n) { return n?n*Math.random():Math.random(); },
-
-  // Random integer from [min,max] (bound inclusive)
-  R : function(min,max) { return min+(Math.random()*(max-min+1))>>0; },
-
-  // Array sum
-  sum : function(a) { for(var sum=0, i=a.length; i--; sum+=a[i] ); return sum; },
-
-  // Object attribute sum
-  sumObj : function(a) { var sum=0; for(var i in a) sum+=a[i]; return sum; },
 
   // Object literal extend.
   extend : function(target , extender) {
@@ -34,5 +58,5 @@ define({
     }
     return target;
   }
-  
+
 });
